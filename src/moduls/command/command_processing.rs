b7724e8cb9a057e03356bef83a9540b1006084c9
anyway <zorix228337@gmail.com>
2025-12-30 11:53:34 +0300

    wrote new command process

---
+++ src/moduls/command/command_processing.rs
@@ -0,0 +1,21 @@
+include!("./command_line.rs");
+
+use std::fs;
+use std::process;
+
+fn process_new_cmd(dir_name: &String) -> Result<(), std::io::Error> {
+//  make a directory
+    fs::create_dir(dir_name)?;
+    process::Command::new("cd").arg(dir_name).output()?;
+    
+//  make a support files and directories
+    fs::File::create_new(".gitignore")?;
+    fs::File::create_new("ShineMT.json")?;
+    process::Command::new("git init").output()?;
+
+    fs::create_dir("src");
+    fs::File::create_new("src/main.shi");
+    
+    process::Command::new("cd").arg("..").output()?;
+    return Result::Ok(());
+}
